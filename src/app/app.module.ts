d9a33c12c5cf33e6d560fa527caa3908748f2afa
Suraj <sonawanesuraj74@gmail.com>
2020-11-20 17:17:19 +0530

    my first

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AboutComponent } from './about/about.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { ImageSliderComponent } from './image-slider/image-slider.component';
+import { JumbotronComponent } from './jumbotron/jumbotron.component';
+import { TeamSectionComponent } from './team-section/team-section.component';
+import { PhilosophyComponent } from './philosophy/philosophy.component';
+import { SocialComponent } from './social/social.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    NavigationComponent,
+    ImageSliderComponent,
+    JumbotronComponent,
+    TeamSectionComponent,
+    PhilosophyComponent,
+    SocialComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FontAwesomeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
